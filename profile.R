b357ea2b58a6ad825304adb95b46b53bb6a1dd40
Hannah Johnson <143128677+hannahrshea@users.noreply.github.com>
2024-04-09 04:33:17 -0400

    Update profile.R - slow code

--- profile.R
+++ profile.R
@@ -1,13 +1,27 @@
 Rprof()
-library(readr)
-library(randomForest)
-setwd("C:/Users/hrj33/Downloads/BZAN542-Files Data Mining/BZAN542-GroupProject")
-project542data <- read_csv("project542data.csv")
-sample_size <- 0.01
-subset_data <- project542data[sample(seq_len(nrow(project542data)), size=floor(sample_size*nrow(project542data))),]
-rf_model <- randomForest(INCWAGE ~ .,
-                         data = training_data,
-                         ntree = 200,
-                         importance = TRUE)
+#Code for Monte Carlo Simulation
+ntrials<-1000000
+win.A<-0
+win.B<-0
+tie<-0
+for(n in 1:ntrials){
+  buzz.A<-sample(0:1,30,replace=TRUE,prob=c(60,40))
+  buzz.B<-(1-buzz.A)
+  correct.A<-sample(0:1,30,replace=TRUE,prob=c(20,80))
+  correct.B<-sample(0:1,30,replace=TRUE)
+  score.A<-sum(buzz.A*correct.A)
+  score.B<-sum(buzz.B*correct.B)
+  if(score.A>score.B){win.A<-win.A+1}
+  if(score.A<score.B){win.B<-win.B+1}
+  if(score.A==score.B){tie<-tie+1}
+}
+
+#Outcomes
+#Chance A wins
+win.A/ntrials
+#Chance B wins
+win.B/ntrials
+#Chance of tie
+tie/ntrials
 Rprof(NULL)
 summaryRprof()
